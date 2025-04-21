4582d0fff95a39e13a143ede58f3a1e34a768f1f
robertovargash <robertovargashdez@gmail.com>
2025-04-21 14:27:15 -0400

    updating blazor app

--- Src/SamplesByPlatforms/Xceed.Blazor.Zip.Sample/Program.cs
+++ Src/SamplesByPlatforms/Xceed.Blazor.Zip.Sample/Program.cs
@@ -1,24 +1,13 @@
-using Xceed.Blazor.Zip.Sample.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Xceed.Blazor.Zip.Sample;
+
 Xceed.Zip.Licenser.LicenseKey = "LICENSE_KEY_PLACEHOLDER";
 
-var builder = WebApplication.CreateBuilder( args );
+var builder = WebAssemblyHostBuilder.CreateDefault( args );
+builder.RootComponents.Add<App>( "#app" );
+builder.RootComponents.Add<HeadOutlet>( "head::after" );
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents();
+builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if( !app.Environment.IsDevelopment() )
-{
-	app.UseExceptionHandler( "/Error", createScopeForErrors: true );
-}
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-	.AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
